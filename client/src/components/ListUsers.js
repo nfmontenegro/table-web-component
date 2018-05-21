b279a055fdbca63f445dfd6ddf1c08073587b6bc
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-05-21 19:36:14 -0400

    add librarys sematic-ui-react

--- client/src/components/ListUsers.js
+++ client/src/components/ListUsers.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Card, Icon, Image, Container } from 'semantic-ui-react'
 
 export const ListUsers = () => (
   <Query
@@ -19,9 +20,16 @@ export const ListUsers = () => (
       if (loading) return <p>Loading ...</p>
       if (error) return <p>Something is wrong! 😕</p>
 
-      return data.listUsers.map(({ firstname, lastname }) => {
-        return <p>{firstname}</p>
-      })
+      return data.listUsers.map(({ firstname, lastname }) => (
+        <Container>
+          <Card>
+            <Card.Content>
+              <Card.Header>{firstname}</Card.Header>
+              <Card.Description>{lastname}</Card.Description>
+            </Card.Content>
+          </Card>
+        </Container>
+      ))
     }}
   </Query>
 )
