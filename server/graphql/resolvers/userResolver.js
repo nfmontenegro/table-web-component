eb396cb3b991ff0bddf8b44336e90e7c4caa7b9c
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-05-23 11:44:54 -0400

    query user

--- server/graphql/resolvers/userResolver.js
+++ server/graphql/resolvers/userResolver.js
@@ -3,14 +3,14 @@ import { authService } from '../../middleware/jwt'
 export default {
   Query: {
     listUsers: async (__, args, { pool, user }) => {
-      const queryDb = 'SELECT * FROM "users"'
+      const queryDb = 'SELECT * FROM users'
       const { rows, fields } = await pool.query(queryDb)
       const payload = authService().signJWT({ rows })
       const response = authService().verifyJWT(payload)
       return rows
     },
     listUser: async (__, { id }, { pool }) => {
-      const queryDb = `SELECT * FROM USERS WHERE id=${id}`
+      const queryDb = `SELECT * FROM users WHERE id=${id}`
       const { rows } = await pool.query(queryDb)
       return rows
     }
